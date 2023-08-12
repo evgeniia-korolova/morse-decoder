10ed43faa1fd26781a382208e1967085318decf3
evgeniia-korolova <jkod@ukr.net>
2023-08-12 14:11:36 +0300

    feat : add code

--- src/index.js
+++ src/index.js
@@ -38,8 +38,31 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  const decoded = [];
+
+  for (let i = 0; i < expr.length; i += 10) {
+    const uncodedLetters = expr.substr(i, 10);
+    if (uncodedLetters === '**********') {
+      decoded.push(' ');
+    } else {
+      const decodedMorseKey = [];
+
+      for (let j = 0; j < uncodedLetters.length; j += 2) {
+        const encodedMorseKey = uncodedLetters.substr(j, 2);
+        if (encodedMorseKey === '10')  decodedMorseKey.push('.');
+        
+        if (encodedMorseKey === '11')  decodedMorseKey.push('-');
+        
+      }
+
+      const morseKey = decodedMorseKey.join('');
+      decoded.push(MORSE_TABLE[morseKey]);
+    }
+  }
+
+  return decoded.join('');
 }
+
 
 module.exports = {
     decode
